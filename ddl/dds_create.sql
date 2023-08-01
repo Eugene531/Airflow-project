7e91970951cb25e5d45bec817f0622da2d7d2ead
Eugene531 <tarov25@mail.ru>
2023-08-01 16:51:30 +0300

    New

--- ddl/dds_create.sql
+++ ddl/dds_create.sql
@@ -1,102 +1,102 @@
--- Создание схемы
+-- Создание схемы "dds"
 CREATE SCHEMA IF NOT EXISTS dds;
 
--- Создание таблицы "shop"
+-- Создание таблицы "shop" для хранения информации о магазинах
 CREATE TABLE IF NOT EXISTS dds.shop (
-    pos VARCHAR(50) PRIMARY KEY,
-    pos_name VARCHAR(50)
+   pos VARCHAR(50) PRIMARY KEY,                -- ID магазина
+   pos_name VARCHAR(50) NOT NULL               -- Название магазина (не может быть пустым)
 );
 
--- Создание таблицы "tran_shop"
+-- Создание таблицы "tran_shop" для хранения информации о транзакциях и связанных с ними магазинах
 CREATE TABLE IF NOT EXISTS dds.tran_shop (
-    transaction_id VARCHAR(50) PRIMARY KEY,
-    pos VARCHAR(50),
-    FOREIGN KEY (pos) REFERENCES dds.shop (pos)
+   transaction_id VARCHAR(50) PRIMARY KEY,     -- ID транзакции
+   pos VARCHAR(50) NOT NULL,                   -- ID магазина, связанный с транзакцией (не может быть пустым)
+   FOREIGN KEY (pos) REFERENCES dds.shop (pos)
 );
 
--- Создание таблицы "brand" с первичным ключом "brand_id"
+-- Создание таблицы "brand" для хранения информации о брендах продуктов
 CREATE TABLE IF NOT EXISTS dds.brand (
-    brand_id VARCHAR(50) PRIMARY KEY,
-    brand VARCHAR(255)
+   brand_id VARCHAR(50) PRIMARY KEY,           -- ID бренда
+   brand VARCHAR(255) NOT NULL                 -- Название бренда (не может быть пустым)
 );
 
--- Создание таблицы "category" с первичным ключом "category_id"
+-- Создание таблицы "category" для хранения информации о категориях продуктов
 CREATE TABLE IF NOT EXISTS dds.category (
-    category_id VARCHAR(50) PRIMARY KEY,
-    category_name VARCHAR(50)
+   category_id VARCHAR(50) PRIMARY KEY,        -- ID категории
+   category_name VARCHAR(50) NOT NULL          -- Название категории (не может быть пустым)
 );
 
--- Создание таблицы "product" с первичным ключом "product_id"
+-- Создание таблицы "product" для хранения информации о продуктах
 CREATE TABLE IF NOT EXISTS dds.product (
-    product_id VARCHAR(50) PRIMARY KEY,
-    name_short VARCHAR(255),
-    category_id VARCHAR(50),
-    pricing_line_id VARCHAR(50),
-    brand_id VARCHAR(50),   
-    FOREIGN KEY (category_id) REFERENCES dds.category (category_id),
-    FOREIGN KEY (brand_id) REFERENCES dds.brand (brand_id)
+   product_id VARCHAR(50) PRIMARY KEY,         -- ID продукта
+   name_short VARCHAR(255) NOT NULL,           -- Название продукта (не может быть пустым)
+   category_id VARCHAR(50),                    -- ID категории продукта
+   pricing_line_id VARCHAR(50),                -- ID линии ценообразования продукта
+   brand_id VARCHAR(50),                       -- ID бренда продукта
+   FOREIGN KEY (category_id) REFERENCES dds.category (category_id),
+   FOREIGN KEY (brand_id) REFERENCES dds.brand (brand_id)
 );
 
--- Создание таблицы "stock"
+-- Создание таблицы "stock" для хранения информации о запасах продуктов
 CREATE TABLE IF NOT EXISTS dds.stock (
-    available_on DATE,
-    product_id VARCHAR(50),
-    pos VARCHAR(50),
-    available_quantity NUMERIC (10,3),
-    cost_per_item NUMERIC (10,3),
-    PRIMARY KEY (available_on, product_id, pos),
-    FOREIGN KEY (pos) REFERENCES dds.shop (pos),
-    FOREIGN KEY (product_id) REFERENCES dds.product (product_id)
+   available_on DATE,                          -- Дата доступности продукта
+   product_id VARCHAR(50),                     -- ID продукта
+   pos VARCHAR(50) NOT NULL,                   -- ID магазина (не может быть пустым)
+   available_quantity NUMERIC (10,3) NOT NULL, -- Доступное количество продукта (не может быть пустым)
+   cost_per_item NUMERIC (10,3) NOT NULL,      -- Цена продукта за единицу (не может быть пустой)
+   PRIMARY KEY (available_on, product_id, pos),
+   FOREIGN KEY (pos) REFERENCES dds.shop (pos),
+   FOREIGN KEY (product_id) REFERENCES dds.product (product_id)
 );
 
--- Создание таблицы "transaction"
+-- Создание таблицы "transaction" для хранения информации о транзакциях продажи продуктов
 CREATE TABLE IF NOT EXISTS dds.transaction (
-    transaction_id VARCHAR(50),
-    product_id VARCHAR(50),
-    recorded_on TIMESTAMP,
-    quantity INTEGER,
-    price INTEGER,
-    price_full INTEGER,
-    order_type_id VARCHAR(50),
-    PRIMARY KEY (transaction_id, product_id),
-    FOREIGN KEY (product_id) REFERENCES dds.product (product_id),
-    FOREIGN KEY (transaction_id) REFERENCES dds.tran_shop (transaction_id)
+   transaction_id VARCHAR(50),                 -- ID транзакции
+   product_id VARCHAR(50),                     -- ID продукта
+   recorded_on TIMESTAMP NOT NULL,             -- Время записи транзакции (не может быть пустым)
+   quantity INTEGER NOT NULL,                  -- Количество продуктов в транзакции (не может быть пустым)
+   price INTEGER NOT NULL,                     -- Цена продукта за единицу в транзакции (не может быть пустой)
+   price_full INTEGER NOT NULL,                -- Полная цена продукта в транзакции (не может быть пустой)
+   order_type_id VARCHAR(50) NOT NULL,         -- Тип транзакции (не может быть пустым)
+   PRIMARY KEY (transaction_id, product_id),
+   FOREIGN KEY (product_id) REFERENCES dds.product (product_id),
+   FOREIGN KEY (transaction_id) REFERENCES dds.tran_shop (transaction_id)
 );
 
--- Создание таблицы "product_errors"
+-- Создание таблицы "product_errors" для хранения ошибок при обработке данных о продуктах
 CREATE TABLE IF NOT EXISTS dds.product_errors (
-    product_id VARCHAR(50),
-    name_short VARCHAR(50),
-    category_id VARCHAR(50),
-    pricing_line_id VARCHAR(50),
-    brand_id VARCHAR(50),
-    name_short_error VARCHAR(50)
+   product_id VARCHAR(50) NOT NULL,            -- ID продукта, в котором возникла ошибка (не может быть пустым)
+   name_short VARCHAR(50),                     -- Название продукта
+   category_id VARCHAR(50),                    -- ID категории продукта
+   pricing_line_id VARCHAR(50),                -- ID линии ценообразования продукта
+   brand_id VARCHAR(50),                       -- ID бренда продукта
+   name_short_error VARCHAR(50) NOT NULL       -- Ошибка в названии продукта (не может быть пустой)
 );
 
--- Создание таблицы "stock_errors"
+-- Создание таблицы "stock_errors" для хранения ошибок при обработке данных о запасах продуктов
 CREATE TABLE IF NOT EXISTS dds.stock_errors (
-    available_on VARCHAR(50),
-    product_id VARCHAR(50),
-    pos VARCHAR(50),
-    available_quantity VARCHAR(50),
-    cost_per_item VARCHAR(50),
-    product_id_error VARCHAR(50),
-    available_quantity_error VARCHAR(50),
-    cost_per_item_error VARCHAR(50),
-    pos_error VARCHAR(50)
+   available_on DATE NOT NULL,                 -- Дата доступности продукта (не может быть пустой)
+   product_id VARCHAR(50) NOT NULL,            -- ID продукта (не может быть пустым)
+   pos VARCHAR(50) NOT NULL,                   -- ID магазина (не может быть пустым)
+   available_quantity VARCHAR(50),             -- Доступное количество продукта (строковый тип данных)
+   cost_per_item VARCHAR(50),                  -- Цена продукта за единицу (строковый тип данных)
+   product_id_error VARCHAR(50) NOT NULL,      -- Ошибка в id продукта (не может быть пустой)
+   available_quantity_error VARCHAR(50),       -- Ошибка в доступном количестве продукта (строковый тип данных)
+   cost_per_item_error VARCHAR(50),            -- Ошибка в цене продукта за единицу (строковый тип данных)
+   pos_error VARCHAR(50)                       -- Ошибка в id магазина (строковый тип данных)
 );
 
--- Создание таблицы "transaction_errors"
+-- Создание таблицы "transaction_errors" для хранения ошибок при обработке данных о транзакциях продажи продуктов
 CREATE TABLE IF NOT EXISTS dds.transaction_errors (
-    transaction_id VARCHAR(50),
-    product_id VARCHAR(50),
-    recorded_on VARCHAR(50),
-    quantity VARCHAR(50),
-    price VARCHAR(50),
-    price_full VARCHAR(50),
-    order_type_id VARCHAR(50),
-    quantity_error VARCHAR(50),
-    price_error VARCHAR(50),
-    product_id_error VARCHAR(50),
-    transaction_id_error VARCHAR(50)
-);
+   transaction_id VARCHAR(50) NOT NULL,        -- ID транзакции (не может быть пустым)
+   product_id VARCHAR(50) NOT NULL,            -- ID продукта (не может быть пустым)
+   recorded_on VARCHAR(50) NOT NULL,           -- Время записи транзакции (не может быть пустым)
+   quantity VARCHAR(50),                       -- Количество продуктов в транзакции (строковый тип данных)
+   price VARCHAR(50),                          -- Цена продукта за единицу в транзакции (строковый тип данных)
+   price_full VARCHAR(50),                     -- Полная цена продукта в транзакции (строковый тип данных)
+   order_type_id VARCHAR(50),                  -- Тип транзакции (строковый тип данных)
+   quantity_error VARCHAR(50),                 -- Ошибка в количестве продуктов (строковый тип данных)
+   price_error VARCHAR(50),                    -- Ошибка в цене продукта за единицу (строковый тип данных)
+   product_id_error VARCHAR(50),               -- Ошибка в id продукта (строковый тип данных)
+   transaction_id_error VARCHAR(50)            -- Ошибка в id транзакции (строковый тип данных)
+); 
--- ddl/dm_create.sql
+++ ddl/dm_create.sql
@@ -1,52 +1,51 @@
--- Создание схемы
-CREATE SCHEMA IF NOT EXISTS dm;
+-- Создание схемы "dm" для хранения данных Data Mart
 
--- Создание таблицы "transactions_group_ymd"
+-- Создание таблицы "transactions_group_ymd" для хранения агрегированных данных по транзакциям, сгруппированным по дате
 CREATE TABLE IF NOT EXISTS dm.transactions_group_ymd (
-    date TEXT,
-    day INTEGER,
-    pos VARCHAR(50),
-    category_name VARCHAR(50),
-    name_short VARCHAR(255),
-    sum_price BIGINT,
-    sum_quantity BIGINT,
-    UPDATE_DATE DATE
+    date TEXT,                      -- Дата
+    day INTEGER,                    -- День недели (числовое значение)
+    pos VARCHAR(50),                -- Уникальный ID магазина (POS)
+    category_name VARCHAR(50),      -- Название категории продукта
+    name_short VARCHAR(255),        -- Название продукта
+    sum_price BIGINT,               -- Цена за день
+    sum_quantity BIGINT,            -- Общее количество проданных продуктов в конкретный день недели
+    UPDATE_DATE DATE                -- Дата обновления записи
 );
 
--- Создание таблицы "average_check"
+-- Создание таблицы "average_check" для хранения среднего чека по магазинам и датам
 CREATE TABLE IF NOT EXISTS dm.average_check (
-    date VARCHAR(10),
-    pos VARCHAR(50),
-    avg NUMERIC(10,2),
-    UPDATE_DATE DATE
+    date VARCHAR(10),               -- Дата
+    pos VARCHAR(50),                -- Уникальный ID магазина (POS)
+    avg NUMERIC(10,2),              -- Средний чек
+    UPDATE_DATE DATE                -- Дата обновления записи
 );
 
--- Создание таблицы "purchases"
+-- Создание таблицы "purchases" для хранения информации о закупках продуктов
 CREATE TABLE IF NOT EXISTS dm.purchases (
-    product_id VARCHAR(50),
-    update_date_in_stock DATE,
-    stock_total NUMERIC(10,2),
-    cost_per_item NUMERIC(10,2),
-    name_short VARCHAR(255),
-    category VARCHAR(50),
-    sold_total NUMERIC(10,2),
-    UPDATE_DATE DATE
+    product_id VARCHAR(50),         -- Уникальный ID продукта
+    update_date_in_stock DATE,      -- Дата последнего обновления запасов продукта
+    stock_total NUMERIC(10,2),      -- Общее количество продукта в запасах
+    cost_per_item NUMERIC(10,2),    -- Цена продукта за единицу
+    name_short VARCHAR(255),        -- Название продукта
+    category VARCHAR(50),           -- Категория продукта
+    sold_total NUMERIC(10,2),       -- Общее количество проданного продукта
+    UPDATE_DATE DATE                -- Дата обновления записи
 );
 
--- Создание таблицы "mean_monthly_product_stats"
+-- Создание таблицы "mean_monthly_product_stats" для хранения средних месячных статистических данных по продуктам
 CREATE TABLE IF NOT EXISTS dm.mean_monthly_product_stats (
-    product_id VARCHAR(50),
-    date VARCHAR(10),
-    quantity NUMERIC(10,2),
-    avg_price NUMERIC(10,2),
-    UPDATE_DATE DATE
+    product_id VARCHAR(50),         -- Уникальный ID продукта
+    date VARCHAR(10),               -- Дата
+    quantity NUMERIC(10,2),         -- Среднее количество продуктов, проданных в месяц
+    avg_price NUMERIC(10,2),        -- Средняя цена продукта в месяц
+    UPDATE_DATE DATE                -- Дата обновления записи
 );
 
--- Создание таблицы "total_stats"
+-- Создание таблицы "total_stats" для хранения общей статистики по продажам
 CREATE TABLE IF NOT EXISTS dm.total_stats (
-    date VARCHAR(10),
-    total_gross NUMERIC(10,2),
-    total_volume NUMERIC(10,2),
-    avg_check NUMERIC(10,2),
-    UPDATE_DATE DATE
+    date DATE,                      -- Дата
+    total_gross NUMERIC(10,2),      -- Общая выручка за месяц
+    total_volume NUMERIC(10,2),     -- Общий объем продаж за месяц
+    avg_check NUMERIC(10,2),        -- Средний чек за месяц
+    UPDATE_DATE DATE                -- Дата обновления записи
 );
